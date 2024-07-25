59e421d42a273b9f2b5539b06f5deb59e6c7f3ea
jatin192003 <jatin192003@gmail.com>
2024-07-25 13:22:35 +0530

    first commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logo = ({width = '100px'}) => {
+    return (
+        <div>
+            <img src="src\img\Vichaar.png" alt="Vichaar" width="100px"/>
+        </div>
+    );
+}
+
+export default Logo;
